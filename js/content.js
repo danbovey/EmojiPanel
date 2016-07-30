danbovey/EmojiPanel

commit 84bb90843d77a757c2aec122522706efcfeee4b0

Fixed #1 - Icon color

js/content.js
@@ -227,7 +227,7 @@ var EmojiPanel = {
     },
     checkForms: function() {
         var self = this;
-        
+
         var forms = document.querySelectorAll('.tweet-form');
         [].forEach.call(forms, function(form) {
             if(!form.classList.contains('EmojiPanel')) {
@@ -268,7 +268,7 @@ var EmojiPanel = {
 
             var icon = document.createElement('span');
             icon.classList.add('Icon', 'Icon--smiley');
-            icon.style.color = window.getComputedStyle(document.querySelector('.TweetBoxExtras-item .Icon')).color;
+            icon.style.color = window.getComputedStyle(tweetBox.querySelector('.TweetBoxExtras-item .Icon')).color;
             btn.appendChild(icon);
 
             var tooltip = document.createElement('span');

commit e37ce718ea9188d512d7f917121489cc2989c1b2

Fixed error caused by 84bb908

js/content.js
@@ -268,7 +268,13 @@ var EmojiPanel = {
 
             var icon = document.createElement('span');
             icon.classList.add('Icon', 'Icon--smiley');
-            icon.style.color = window.getComputedStyle(tweetBox.querySelector('.TweetBoxExtras-item .Icon')).color;
+
+            var color = '#2F9AC2';
+            var otherIcon = form.querySelector('.TweetBoxExtras-item .Icon');
+            if(otherIcon) {
+                color = window.getComputedStyle(otherIcon).color
+            }
+            icon.style.color = color;
             btn.appendChild(icon);
 
             var tooltip = document.createElement('span');