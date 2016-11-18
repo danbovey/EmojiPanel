3f36f367c98647a4470347ef5b827daee276bba8
Dan Bovey <dan@danbovey.uk>
2016-11-18 00:48:51 +0000

    1.2.5

--- js/storage.js
+++ js/storage.js
@@ -13,10 +13,7 @@ let options = {
 };
 let json = {};
 
-const notifications = [{
-    id: 2,
-    message: '<strong>99% working</strong><br>Everything is back to normal. The only bug remaining is support for more than one skin tone emoji at once. It should be fixed soon <svg viewBox="0 0 20 20" width="20"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#1f44d"></use></svg>.'
-}];
+const notifications = [];
 
 const Storage = {
     load: () => {
