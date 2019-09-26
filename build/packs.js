c1ba9c6ede0320742044db4bb969a3ea40754254
Francisco Madeira <xico2k@gmail.com>
2019-09-26 10:52:36 +0100

    upgrade packages and gulp to v4 (#33)
    * upgrade packages and gulp to v4
    
    * since tarball from github is not available anymore we will need to download it with the github-download package

--- build/packs.js
+++ build/packs.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
+const del = require('del');
 require('colors');
 
 const buildJson = require('./json');
 const generateSprite = require('./sprite');
+const ghdownload = require('github-download');
 
 const packs = [
     {
         name: 'twemoji',
-        location: 'node_modules/twemoji',
-        findFile: unicode => `node_modules/twemoji/2/svg/${unicode}.svg`
+        location: 'build/twemoji',
+        findFile: unicode => `build/twemoji/2/svg/${unicode}.svg`
     },
     {
         name: 'emojione',
@@ -20,7 +22,7 @@ const packs = [
 const build = pack => {
     try {
         fs.statSync(pack.location);
-        
+
         return generateSprite(pack);
     } catch(err) {
         return Promise.reject(err);
@@ -35,9 +37,17 @@ const buildNext = () => {
             .then(() => buildNext())
             .catch(err => console.log(err));
     } else {
+        del.sync(['build/twemoji'], { force:true });
         console.log('Complete!'.inverse.green)
     }
 };
 
+const downloadTwemoji = () => {
+    ghdownload({user: 'twitter', repo: 'twemoji', ref: 'gh-pages'}, process.cwd() + '/build/twemoji')
+        .on('end', function() {
+            buildNext();
+        });
+}
+
 buildJson();
-module.exports = buildNext();
+module.exports = downloadTwemoji();
